7aa00b575262ba214725c1a9a032829fb358395f
Fabien <fabien.chevrey@musicworlmedia.com>
2020-11-12 14:35:17 +0100

    ex01

---
+++ d01/ex01/ex01/eValue.swift
@@ -0,0 +1,14 @@
+//
+//  eValue.swift
+//  ex00
+//
+//  Created by Fabien CHEVREY on 12/11/2020.
+//
+
+import Foundation
+
+enum eValue :Int
+{
+        case As, King, Queen, Jack, ten, nine, height, seven, six, five, four, three, two, one
+    static let allValues = [As, King, Queen, Jack, ten, nine, height, seven, six, five, four, three, two, one]
+}
